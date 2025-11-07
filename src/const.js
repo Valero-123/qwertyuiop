e328fb9c6e8e0bdc1ed2d4c4fef7c903a04d688d
Valero-123 <era_ich@mail.ru>
2025-11-07 11:32:39 +0300

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,15 +1,13 @@
 const Genre = {
     FICTION : `fiction`,
     SCIENCE: `science`,
-    FANTASY: `fantasy`,
-    BIOGRAPHY: `biography`
 }
 
 const GenreLabel = {
-    [Status.FICTION]: `Художественная`,
-    [Status.SCIENCE]: `Научная`,
-    [Status.FANTASY]: `Фантастика`,
-    [Status.BIOGRAPHY]: `Биография`
+    [Status.FICTION]: `Выполнено`,
+    [Status.SCIENCE]: `Не выполнено`,
+
 }
+
 
 export {Genre, GenreLabel};
